7fa4473ea281251ba0ebb78864ed6bd3c727589f
RyanSample <sampleryan1@gmail.com>
2016-09-14 12:45:43 -0500

    Initial Commit

---
+++ RealMax/RealMax/Models/Listing.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+//TODO:
+/* http://www.asp.net/web-api/overview/data/using-web-api-with-entity-framework/part-2
+ * http://www.asp.net/web-api/overview/data/using-web-api-with-entity-framework/part-3
+ * http://www.asp.net/web-api/overview/data/using-web-api-with-entity-framework/part-4
+ * http://www.asp.net/mvc/overview/getting-started/getting-started-with-ef-using-mvc/migrations-and-deployment-with-the-entity-framework-in-an-asp-net-mvc-application
+ * */
+namespace RealMax.Models
+{
+    public class Listing
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string HouseNumber { get; set; }
+
+        public string StreetName { get; set; }
+
+        public string ApartmentNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public int Price { get; set; }
+
+        [Display(Name = "Bedrooms")]
+        public int Bed { get; set; }
+
+        [Display(Name = "Bathrooms")]
+        public int Bath { get; set; }
+
+        //Foreign Key
+        public int RealtorId { get; set; }
+
+        public virtual Realtor Realtor { get; set; }
+
+        public string ExtraFeatures { get; set; }
+
+        [Display(Name = "Sq. Feet")]
+        public int SquareFeet { get; set; }
+
+        public int LotSize { get; set; }
+
+
+    }
+}
---
+++ RealMax/RealMax/Models/Realtor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealMax.Models
+{
+    public class Realtor
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+        
+        public string Bio { get; set; } 
+
+        public virtual ICollection<Listing> Listings { get; set; }
+    }
+}
