5d2aee96ef4a2896c3fb8f5ce03854b93491601d
RyanSample <sampleryan1@gmail.com>
2016-11-15 16:04:02 -0600

    Started working on the landing page. Added a new model so that I could use two existing models on one view.

---
+++ RealMax/RealMax/Models/MainPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealMax.Models
+{
+    public class MainPage
+    {
+        public Listing Listings { get; set; }
+        public Realtor Realtors { get; set; }
+    }
+}
