4286fd21fb42f05c7c27777120696f30004da55e
RyanSample <sampleryan1@gmail.com>
2016-12-05 23:19:38 -0600

    created a javascript function that handles creating uniform heights in the listing page instead of setting it to some arbitrary height.

---
+++ RealMax/RealMax/Scripts/StandardizeHeight.js
@@ -0,0 +1,16 @@
+﻿//this code will make sure that each listing-item is the same height
+$(document).ready(function () {
+    var largestHeight = 0;
+    var elements = $(".listing-item")
+    elements.each(function (index) {
+        var elementHeight = $(this).height();
+        if (largestHeight < elementHeight) {
+            largestHeight = elementHeight
+        }
+    });
+
+    elements.each(function (index) {
+        $(this).height(largestHeight);
+    });
+    //console.log("StandardizeHeight.js ran");
+});
