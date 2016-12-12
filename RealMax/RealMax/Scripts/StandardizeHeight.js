810e90b0f0b64b2704280a8bbc2800741e3e3372
RyanSample <sampleryan1@gmail.com>
2016-12-12 16:48:45 -0600

    fixed an issue with listing items displaying text outside of their border due to the height being set to low.

--- RealMax/RealMax/Scripts/StandardizeHeight.js
+++ RealMax/RealMax/Scripts/StandardizeHeight.js
@@ -1,16 +1,27 @@
 ﻿//this code will make sure that each listing-item is the same height
+function getLargestHeight(element, largestHeight){
+    var elementHeight = $(element).height();
+    if (elementHeight > largestHeight) {
+        return elementHeight;
+    }
+    return largestHeight;
+}
+
 $(document).ready(function () {
     var largestHeight = 0;
     var elements = $(".listing-item")
+    
+    console.log("Number of elements " + elements.length);
     elements.each(function (index) {
-        var elementHeight = $(this).height();
-        if (largestHeight < elementHeight) {
-            largestHeight = elementHeight
-        }
+        $(this).ready(largestHeight = getLargestHeight(this, largestHeight));
+        //console.log("largest height: " + largestHeight + " this element's height: " + $(this).height() + " item # " + index);
     });
 
+    //console.log("largest height: " + largestHeight + " px");
+
     elements.each(function (index) {
+        //console.log("Element # " + index);
         $(this).height(largestHeight);
     });
-    //console.log("StandardizeHeight.js ran");
+//    console.log("StandardizeHeight.js ran");
 });
